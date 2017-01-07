3b92bb0ea4470942515812bf4145a29a78b32e8c
aaqib29292 <aaqib29292@gmail.com>
2017-01-08 00:43:07 +0530

    added email-password authentication using firebase to the app

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    webserver = require('gulp-webserver');
+    webserver = require('gulp-webserver'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer');
+
+
+    var autoprefixerOptions = {
+      browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    };
+
+
 
 gulp.task('js', function() {
   gulp.src('builds/dev/js/**/*');
@@ -14,11 +24,21 @@ gulp.task('css', function() {
   gulp.src('builds/dev/css/*.css');
 });
 
+
+gulp.task('sass', function () {
+  return gulp.src('builds/dev/css/sass/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(sourcemaps.write('builds/dev/css/'))
+    .pipe(gulp.dest('builds/dev/css/'));
+});
+
 gulp.task('watch', function() {
   gulp.watch('builds/dev/js/**/*', ['js']);
   gulp.watch('builds/dev/css/*.css', ['css']);
-  gulp.watch(['builds/dev/*.html',
-    'builds/dev/views/*.html'], ['html']);
+  gulp.watch(['builds/dev/*.html', 'builds/dev/views/*.html'], ['html']),
+  gulp.watch('builds/dev/css/sass/*.scss', ['sass']);
 });
 
 gulp.task('webserver', function() {
@@ -29,4 +49,4 @@ gulp.task('webserver', function() {
     }));
 });
 
-gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
+gulp.task('default', ['sass', 'watch', 'html', 'js', 'css',  'webserver']);
